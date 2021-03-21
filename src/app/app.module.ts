83ca12fb3f9ae31712e4b8fc637644f70092dd5c
kevin-a1 <kevin.llivichuzhca.est@gmail.com>
2021-03-21 10:46:07 -0500

    Componente login-correo creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
+import { LoginCorreoComponent } from './login-correo/login-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroUsuarioComponent
+    RegistroUsuarioComponent,
+    LoginCorreoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login-correo/login-correo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-correo',
+  templateUrl: './login-correo.component.html',
+  styleUrls: ['./login-correo.component.css']
+})
+export class LoginCorreoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
