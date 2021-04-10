04b2a27df3b0b20fe7a0186a4657ebea2fd9db8e
Andrés <freddy.sapatanga.est@tecazuay.edu.ec>
2021-04-10 18:11:24 -0500

    Saved as backup for rename administradorservice.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { LoginCorreoComponent } from './components/login-correo/login-correo.com
 import { LoginPasswordComponent } from './components/login-password/login-password.component';
 import { LoginAdminComponent } from './components/login-admin/login-admin.component';
 import { RevisarCasoComponent } from "./components/revisar-caso/revisar-caso.component";
+import { AdministradoresComponent } from './components/administradores/administradores.component';
 const routes: Routes = [
   { path: 'registro-usuario', component:  RegistroUsuarioComponent },
   { path: 'login', component: LoginCorreoComponent},
   { path: 'login-password', component: LoginPasswordComponent },
   { path: 'login-admin', component: LoginAdminComponent},
+  { path: 'administradores', component: AdministradoresComponent},
   {path: 'casos', component: RevisarCasoComponent}
 ];
 
--- src/app/components/administradores/administradores.component.ts
+++ src/app/components/administradores/administradores.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CargarScriptsService } from '../../services/cargar-scripts.service';
+import { Administrador } from '../../models/administrador';
 
 @Component({
   selector: 'app-administradores',
@@ -8,11 +9,17 @@ import { CargarScriptsService } from '../../services/cargar-scripts.service';
 })
 export class AdministradoresComponent implements OnInit {
 
-  constructor(private cargarScriptsService: CargarScriptsService) {
+  list_administradores: Administrador[] = [];
+
+  constructor(private cargarScriptsService: CargarScriptsService, ) {
     cargarScriptsService.load_js("administradores.component.js");
   }
 
   ngOnInit(): void {
+  }
+
+  readAllAdmins() {
+    
   }
 
 }
